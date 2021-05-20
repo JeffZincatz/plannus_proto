15370ef2e7d9f12046df0e45a63ce7b62ac20dce
Bernardus Krishna <krishnabernardus@gmail.com>
2021-05-20 14:08:24 +0800

    adding MyProfile page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:plannus_proto/Init.dart';
 import 'package:plannus_proto/LogIn.dart';
 import 'package:plannus_proto/SignUp.dart';
 import 'package:plannus_proto/Home.dart';
+import 'package:plannus_proto/MyProfile.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -12,6 +13,7 @@ void main() {
       '/login': (context) => LogIn(),
       '/signup': (context) => SignUp(),
       '/home': (context) => Home(),
+      '/myProfile': (context) => MyProfile(),
     },
   ));
 }
