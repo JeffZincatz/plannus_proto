df1c3234876e2c5c05f937da9c7c2573bfa5ef0f
Bernardus Krishna <krishnabernardus@gmail.com>
2021-05-20 23:30:49 +0800

    Added resizeToAvoidBottomInset: false,

--- lib/LogIn.dart
+++ lib/LogIn.dart
@@ -7,6 +7,7 @@ class LogIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: PresetColors.background,
       body: SafeArea(
 
